eb44679d23173f017376b664d30370f099d7ec9c
eduman13 <eduman13@gmail.com>
2020-07-24 21:35:09 +0200

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthUtils } from './utils/auth-utils';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'final-project';
+
+  constructor(private router: Router){
+
+  }
+  ngOnInit(): void {
+    if (!AuthUtils.isLogged()){
+      this.router.navigate(['login']);
+    }
+  }
 }
